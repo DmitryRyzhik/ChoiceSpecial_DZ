8e152046d700cb67abcc4111e744ae1ac139654e
DmitryRyzhik <rdssdr03@gmail.com>
2023-03-26 23:08:13 +0800

    Программа

---
+++ 1/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+var result = new string[array.Length];
+var i = 0;
+foreach (var value in array)
+{
+if(value.Length <=3)
+{
+result[i]=value;
+i++;
+}
+}
+System.Console.Write(string.Join(Environment.NewLine, result, 0, i));
